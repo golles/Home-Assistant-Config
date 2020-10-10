7aafc5c34a1967283dcf34d2c9bbff6d38a8c004
golles <dev@golles.nl>
2020-10-10 12:31:21 +0000

    Remove custom header

--- www/update_js_resources.sh
+++ www/update_js_resources.sh
@@ -4,7 +4,6 @@ RESOURCES=$(cat <<'EOF'
 auto-entities.js                https://raw.githubusercontent.com/thomasloven/lovelace-auto-entities/master/auto-entities.js
 battery-state-card.js           https://github.com/maxwroc/battery-state-card/releases/latest/download/battery-state-card.js
 button-card.js                  https://www.github.com/custom-cards/button-card/releases/latest/download/button-card.js
-custom-header.js                https://github.com/maykar/custom-header/releases/latest/download/custom-header.js
 fold-entity-row.js              https://raw.githubusercontent.com/thomasloven/lovelace-fold-entity-row/master/fold-entity-row.js
 mini-graph-card-bundle.js       https://github.com/kalkih/mini-graph-card/releases/latest/download/mini-graph-card-bundle.js
 mini-media-player-bundle.js     https://github.com/kalkih/mini-media-player/releases/latest/download/mini-media-player-bundle.js
