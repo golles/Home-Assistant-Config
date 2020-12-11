201b6c3a0c1b4e5013dd8909f1b8ab027f9c7326
golles <dev@golles.nl>
2020-12-11 20:45:51 +0000

    Only change volume/source when it needs to be changed

--- custom_scripts/lg_sl10.py
+++ custom_scripts/lg_sl10.py
@@ -1,11 +1,47 @@
 import sys
 import temescal
+import time
 
-def setSourceAndVolume(ip, source, volume):
-  "Set the source and volume on the soundbar"
+class LGSoundbar:
+  def __init__(self, ip):
+    '''Initialize a connection to the soundbar.'''
+    self._speaker = temescal.temescal(ip, callback=self.handle_callback)
+    self._volume = None
+    self._source = None
 
-  speaker = temescal.temescal(ip)
-  speaker.set_func(source)
-  speaker.set_volume(volume)
+    self._speaker.get_info()
+    self._speaker.get_func()
 
-setSourceAndVolume(sys.argv[1], sys.argv[2], sys.argv[3])
+    time.sleep(1) # Sleep a little to get the data.
+
+  def setSource(self, source):
+    '''Set the source of the soundbar.'''
+    if self._source is None:
+      # We don't know the current source, let's serve the request.
+      self._speaker.set_func(source)
+    elif not self._source is None and not self._source is int(source):
+      self._speaker.set_func(source)
+
+  def setVolume(self, volume):
+    '''Set the volume of the soundbar.'''
+    if self._volume is None:
+      # We don't know the current volume, let's serve the request.
+      self._speaker.set_volume(volume)
+    elif not self._volume is None and not self._volume is int(volume):
+      self._speaker.set_volume(volume)
+
+  def setSourceAndVolume(self, source, volume):
+    '''Set the source and volume of the soundbar.'''
+    self.setSource(source)
+    self.setVolume(volume)
+
+  def handle_callback(self, callback):
+    '''Handle callbacks from the soundbar.'''
+    data = callback['data']
+    if 'i_vol' in data:
+        self._volume = int(data['i_vol'])
+    if 'i_curr_func' in data:
+        self._source = int(data['i_curr_func'])
+
+soundbar = LGSoundbar(sys.argv[1])
+soundbar.setSourceAndVolume(sys.argv[2], sys.argv[3])
